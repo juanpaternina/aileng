cec9417cfab5406a2865c0751669d7a2d35150af
Juan Pablo Paternina <juan.paternina@gmail.com>
2025-02-09 20:48:46 +0000

    Refactor CI/CD workflow and update HomeScreen component; enable KVM group permissions, replace Button with TouchableOpacity, and specify package manager in package.json

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,10 @@
-import { Image, StyleSheet, Platform, Button } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
@@ -16,7 +22,9 @@ export default function HomeScreen() {
         />
       }
     >
-      <Button title="Press me" />
+      <TouchableOpacity>
+        <Text>Press me</Text>
+      </TouchableOpacity>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
